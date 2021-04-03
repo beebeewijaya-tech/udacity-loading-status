ee3bc2015eb99c679aff7f91cd77ba1234697a92
Bee Bee Wijaya <beebeewijaya@gmail.com>
2021-04-04 01:51:19 +0700

    FINISH: Test

--- starter/app/src/main/java/com/udacity/DetailActivity.kt
+++ starter/app/src/main/java/com/udacity/DetailActivity.kt
@@ -1,8 +1,13 @@
 package com.udacity
 
+import android.app.NotificationManager
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.content.ContextCompat
+import com.udacity.MainActivity.Companion.FILE_NAME
+import com.udacity.MainActivity.Companion.STATUS
 import kotlinx.android.synthetic.main.activity_detail.*
+import kotlinx.android.synthetic.main.content_detail.*
 
 class DetailActivity : AppCompatActivity() {
 
@@ -10,6 +15,19 @@ class DetailActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_detail)
         setSupportActionBar(toolbar)
+
+        val notificationManager = ContextCompat.getSystemService(applicationContext, NotificationManager::class.java) as NotificationManager
+        notificationManager.cancelAll()
+
+        val fileName = intent.getStringExtra(FILE_NAME)
+        val status = intent.getStringExtra(STATUS)
+
+        file_name_value.text = fileName
+        status_value.text = status
+
+        detail_button.setOnClickListener {
+            finish()
+        }
     }
 
 }
